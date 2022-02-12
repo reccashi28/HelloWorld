935a3f9e87159aa40f61c246eb52cd714bc9ccaa
Rebecca Meyer <faustorebeccabsit@gmail.com>
2022-02-12 09:17:14 +0100

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,25 @@
+public class Hello {
+    public static void main(String[] args) {
+        String name = "Joe";
+        int score = 100;
+        int ranked = getRanking(score);
+        System.out.println( name + " , you are Ranked " + ranked + " in the leader board." );
+        name = "Wilhelm";
+        score = 950;
+        ranked = getRanking(score);
+        System.out.println( name + " , you are Ranked " + ranked + " in the leader board." );
+    }
+
+    public static int getRanking(int score){
+        if(score >= 1000){
+            return 1;
+        } else if(score >= 500){
+            return 2;
+        } else {
+           return 3;
+        }
+
+    }
+}
+
+
