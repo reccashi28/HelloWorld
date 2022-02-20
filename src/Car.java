cf1282672b52251f1ab921ced6a4037c82e81ef0
Rebecca Meyer <faustorebeccabsit@gmail.com>
2022-02-20 20:21:53 +0100

    learn classes and encapsulation

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    private int doors;
+    private int wheels;
+    public String model;
+    public String engine;
+    public String color;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+}
