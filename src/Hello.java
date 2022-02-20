4f6c6441246166636065bcbb6bbe7a2ce0375098
Rebecca Meyer <faustorebeccabsit@gmail.com>
2022-02-20 20:49:06 +0100

    created simpleCalculator

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         Car ferrari = new Car();
+        Car porsche = new Car();
         ferrari.setModel("Ivory");
+        porsche.setModel("Apricot");
         System.out.println("Your car model is this: " + ferrari.getModel());
+        System.out.println("Your car model is this: " + porsche.getModel());
     }
 
 }
