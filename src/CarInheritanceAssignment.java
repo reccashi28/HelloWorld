5296c8865cb101385f3779a5c42a3f76c9134404
Rebecca Meyer <faustorebeccabsit@gmail.com>
2022-03-04 22:22:13 +0100

    practice inheritance

---
+++ src/CarInheritanceAssignment.java
@@ -0,0 +1,8 @@
+public class CarInheritanceAssignment extends Vehicle {
+    private String steering;
+
+    public CarInheritanceAssignment(String type, String steering) {
+        super(type);
+        this.steering = steering;
+    }
+}
